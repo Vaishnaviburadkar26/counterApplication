c747013547d43ce0b913d8f4bcb8eeb3d1473672
unknown <@youexample.com>
2024-12-18 19:42:05 +0530

    first commit

---
+++ counter.js
@@ -0,0 +1,39 @@
+
+let counterEl=document.getElementById("counter");
+let count=0;
+
+
+function onDecCounter(){
+
+      count=count-1;
+
+      console.log(count);
+
+      counterEl.textContent=count;
+
+      if(counter<0){
+
+        counter.document.style.color=red;
+
+
+      }
+
+      else{
+
+      }
+    
+
+}
+
+ function onIncCounter(){
+
+    count=count+1;
+
+      console.log(count);
+
+      counterEl.textContent=count;
+
+
+ }
+
+ 
